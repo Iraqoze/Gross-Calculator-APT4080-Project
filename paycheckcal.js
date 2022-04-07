fa8fad5d086c832c25207233b153286194445af3
Lionel Irakoze <iraqoze.lionel@gmail.com>
2022-04-07 08:37:32 +0300

    Refactored PayCheck Calculation functionality

--- paycheckcal.js
+++ paycheckcal.js
@@ -3,75 +3,75 @@ function fetchDeductions(grossIncome) {
     let nhif = 0;
     let deduction = 0;
 
-    if(grossIncome === 5999 || grossIncome < 5999) {
+    if (grossIncome === 5999 || grossIncome < 5999) {
         nhif = 150;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 6000 || grossIncome < 7999) {
+    else if (grossIncome === 6000 || grossIncome < 7999) {
         nhif = 300;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 8000 || grossIncome < 11999) {
+    else if (grossIncome === 8000 || grossIncome < 11999) {
         nhif = 400;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
 
-    else if(grossIncome === 12000 || grossIncome <= 14999) {
+    else if (grossIncome === 12000 || grossIncome <= 14999) {
         nhif = 500;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
 
-    else if(grossIncome === 15000 || grossIncome <= 19999) {
-        nhif = 6000;
-        deduction = nssf + nhif ;
+    else if (grossIncome === 15000 || grossIncome <= 19999) {
+        nhif = 600;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 20000 || grossIncome <= 24999) {
+    else if (grossIncome === 20000 || grossIncome <= 24999) {
         nhif = 750;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 25000 || grossIncome <= 29999) {
+    else if (grossIncome === 25000 || grossIncome <= 29999) {
         nhif = 850;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 30000 || grossIncome <= 34999) {
-        nhif = 300;
-        deduction = nssf + nhif ;
+    else if (grossIncome === 30000 || grossIncome <= 34999) {
+        nhif = 900;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 35000 || grossIncome <= 39000) {
+    else if (grossIncome === 35000 || grossIncome <= 39999) {
         nhif = 950;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 40000 || grossIncome <= 44999) {
+    else if (grossIncome === 40000 || grossIncome <= 44999) {
         nhif = 1000;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 45000 || grossIncome <= 49999) {
+    else if (grossIncome === 45000 || grossIncome <= 49999) {
         nhif = 1100;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 50000 || grossIncome <= 59999) {
-        nhif = 12000;
-        deduction = nssf + nhif ;
+    else if (grossIncome === 50000 || grossIncome <= 59999) {
+        nhif = 1200;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 60000 || grossIncome <= 69999) {
+    else if (grossIncome === 60000 || grossIncome <= 69999) {
         nhif = 1300;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 70000 || grossIncome <= 79999) {
+    else if (grossIncome === 70000 || grossIncome <= 79999) {
         nhif = 1400;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 80000 || grossIncome <= 89999) {
+    else if (grossIncome === 80000 || grossIncome <= 89999) {
         nhif = 1500;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
-    else if(grossIncome === 90000 || grossIncome <= 99999) {
+    else if (grossIncome === 90000 || grossIncome <= 99999) {
         nhif = 1600;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
     else {
         nhif = 17000;
-        deduction = nssf + nhif ;
+        deduction = nssf + nhif;
     }
 
     return deduction;
@@ -94,23 +94,23 @@ function paycheckcal(basicSalary, bonuses) {
     let deductions = fetchDeductions(grossIncome);
 
 
-    if(grossIncome <= 12298) {
+    if (grossIncome >= 0 && grossIncome <= 12298) {
         taxableIncome = grossIncome - deductions;
         payee = taxableIncome * upto12298;
         payeeAfterReflief = payee - relief;
         netPay = grossIncome - payeeAfterReflief;
-    } else if ( grossIncome <= 23885) {
+    } else if (grossIncome >= 12299 && grossIncome <= 23885) {
         taxableIncome = grossIncome - deductions;
         payee = taxableIncome * upto23885;
         payeeAfterReflief = payee - relief;
         netPay = grossIncome - payeeAfterReflief;
-    } else if (grossIncome <= 35472) {
+    } else if (grossIncome >= 23886 && grossIncome <= 35472) {
         taxableIncome = grossIncome - deductions;
         payee = taxableIncome * upto35472;
         payeeAfterReflief = payee - relief;
         netPay = grossIncome - payeeAfterReflief;
     }
-    else if (grossIncome <= 47059) {
+    else if (grossIncome >= 35473 && grossIncome <= 47059) {
         taxableIncome = grossIncome - deductions;
         payee = taxableIncome * upto47059;
         payeeAfterReflief = payee - relief;
@@ -123,8 +123,8 @@ function paycheckcal(basicSalary, bonuses) {
         netPay = grossIncome - payeeAfterReflief;
     }
 
-    return "Total Deductions (NHIF & NSSF) = " + deductions + "  Taxable Income = "
+    return "Total Deductions (NHIF & NSSF) = " + deductions + " Taxable Income = "
         + taxableIncome + " Take Home (Net Pay) " + netPay + " Total Tax paid = " + payeeAfterReflief;
 }
 
-module.exports = { paycheckcal };
+module.exports = { paycheckcal, fetchDeductions };
